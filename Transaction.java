2b0b56a977591b410a7ef4158360b751f02fbf32
Evan Chen <echen26@students.polytechnic.org>
2024-02-08 14:35:43 -0800

    Created fields and constructors for Transaction

--- Transaction.java
+++ Transaction.java
@@ -1,3 +1,9 @@
 public class Transaction {
-    
+    private String date;
+    private int amount;
+
+    public Transaction(String myDate, int myAmount){
+        date = myDate;
+        amount = myAmount;
+    }
 }
