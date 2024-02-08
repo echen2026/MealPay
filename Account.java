ff58377d07a270e28350f47a6f97b2ae050702c7
Warren Peng <wpeng26@students.polytechnic.org>
2024-02-08 15:08:28 -0800

    Constructoy

--- Account.java
+++ Account.java
@@ -3,6 +3,16 @@ import java.util.ArrayList;
 public class Account {
     private ArrayList<Transaction> list;
     private double balance;
+    private int idNumber;
+    private static int idgen = 1;
+    private String name;
     
 
+    public Account(String myName){
+        list = new ArrayList<Transaction>();
+        name = myName;
+        balance = 0;
+        idNumber = idgen;
+        idgen++;
+    }
 }
