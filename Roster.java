09e78bce0caf37874c4aec5eadcd8b6aef8bf781
Evan Chen <echen26@students.polytechnic.org>
2024-02-08 19:19:40 -0800

    Roster fields and constructors

--- Roster.java
+++ Roster.java
@@ -1,3 +1,14 @@
+import java.util.*;
+
 public class Roster {
-    
+    /* fields */
+    private ArrayList<Account> students;
+
+    /* constructors */
+    public Roster(){
+        students = new ArrayList<Account>();
+    }
+
+    /* methods */
+
 }
